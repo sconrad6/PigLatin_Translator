8c4d5a0d7dedb0da8433599ebf28a4f44e72db0f
Savanah Conrad <sibgym@yahoo.com>
2019-07-19 15:46:57 -0400

    First commit

---
+++ Pig_Latin/PigLatin.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Pig_Latin
+{
+    public class PigLatin
+    {
+        private readonly string userWord;
+
+        public PigLatin(string userWord)
+        {
+            this.userWord = userWord;
+        }
+
+        // Create a method to find the index of the first vowel
+        public int VowelIndexer()
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            //int vowelIndexLocation;
+              
+                foreach (char letter in userWord)
+                {
+                    if (userWord.IndexOfAny(vowels) != -1)
+                    {
+                        return userWord.IndexOfAny(vowels);
+                    } 
+                }
+            return 0;
+            }
+        
+
+        // If VowelIndexer return something other than 0, substrings will be made before and after
+        //the 
+        public string ConsonantMover()
+        {
+            string wordStart;
+            string wordEnd;
+            int vowelLocation = VowelIndexer();
+
+                if (vowelLocation != 0)
+                {
+                    wordEnd = userWord.Substring(0, vowelLocation);
+                    wordStart = userWord.Substring(vowelLocation);
+                    return wordStart + wordEnd + "ay";
+                }
+            return userWord + "way";
+        }
+
+        public bool Validator()
+        {
+            foreach (char letter in userWord)
+            {
+                if (char.IsLetter(letter))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public string CanTranslate()
+        {
+            Regex wordCharacters = new Regex("[0-9]");
+
+            if (wordCharacters.IsMatch(userWord))
+            {
+                return userWord;
+            }
+            return null;
+          
+        }
+
+    }
+
+}
+
+
+
+
+
+
---
+++ Pig_Latin/PigLatinTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Pig_Latin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Pig Latin Translator!");
+            
+            string userChoice  = "y";
+            do
+            {
+                Console.WriteLine("Please enter a word:");
+                string userWord = Console.ReadLine().ToLower();
+
+                PigLatin pig1 = new PigLatin(userWord);
+
+                if (pig1.Validator())
+                {
+                    Console.WriteLine(pig1.ConsonantMover());
+                }
+                else
+                {
+                    Console.WriteLine("Invalid. Please try again");
+                    continue;
+                }
+
+                //Ask user if they want to enter another word
+                Console.WriteLine("Would you like to enter another word? Y/N");
+                userChoice = Console.ReadLine().ToLower();
+
+
+            } while (userChoice == "y");
+            Console.WriteLine("Hope your enjoyed translating!");
+        }
+    }
+}
